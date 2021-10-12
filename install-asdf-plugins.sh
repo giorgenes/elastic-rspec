cf34e5fbf942968e9f3a6b80bee977b8661e6f76
Giórgenes Gelatti <giorgenes@gmail.com>
2021-10-12 20:21:16 +1100

    Create install-asdf-plugins.sh

---
+++ install-asdf-plugins.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+. $HOME/.bashrc.d/asdf.sh
+asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
+asdf plugin-add java
+asdf plugin-add elasticsearch https://github.com/asdf-community/asdf-elasticsearch.git
+
