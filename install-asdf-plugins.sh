d9c803d11a72d46d045ca02d3d6030bc97994640
giorgenes <giorgenes@gmail.com>
2021-10-12 10:46:04 +0000

    remove unused script

--- install-asdf-plugins.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-. $HOME/.bashrc.d/asdf.sh
-asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
-asdf plugin-add java
-asdf plugin-add elasticsearch https://github.com/asdf-community/asdf-elasticsearch.git
-
